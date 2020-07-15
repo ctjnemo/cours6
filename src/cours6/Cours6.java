19b76a060b03e042fba292134fd4abbb2c5e41d0
Tianjiao Chen <ctjnemo@gmail.com>
2020-07-15 16:01:05 -0400

    version test

---
+++ src/cours6/Cours6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cours6;
+
+/**
+ *
+ * @author 15145
+ */
+public class Cours6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour "+args[1]+", " +args[0]);
+    }
+    
+}
